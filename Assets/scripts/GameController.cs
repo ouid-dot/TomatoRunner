9265a32da1d823f36e473199d6805941ec696949
sheepwool <114780765+sheepwool@users.noreply.github.com>
2025-11-16 14:00:24 -0800

    Animation and Game Resolution Update
    - Added death animation
    - Made the other animations playable
    - Fixed screen size / resolution issue

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 public class GameController : MonoBehaviour
 {
     Vector2 startPos;
     private AudioManager audioManager;
+    private Animator animator;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
@@ -11,23 +14,39 @@ public class GameController : MonoBehaviour
         audioManager = GameObject.FindGameObjectWithTag("audio").GetComponent<AudioManager>();
     }
 
+
     private void Start()
     {
         startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("enemy"))
         {
+            // To switch to player death animation
+            animator.SetBool("die", true);
+
+            // Disable player input
+            GetComponent<PlayerJump>().enabled = false;
+            GetComponent<Player>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<CapsuleCollider2D>().enabled = false;
+
+            // Player death
             Die();
         }
     }
 
-    void Die()
+    private void MiniJump()
     {
-        Respawn();
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 5f);
+    }
 
+    private void PlaySoundEffect()
+    {
         // Use the instance to play the sound
         if (audioManager != null)
         {
@@ -39,8 +58,27 @@ public class GameController : MonoBehaviour
         }
     }
 
-    void Respawn()
+    void Die()
     {
+        MiniJump();
+        PlaySoundEffect();
+        StartCoroutine(Respawn());
+    }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(1f);
         transform.position = startPos;
+
+        animator.SetBool("die", false);
+
+        MiniJump();
+
+        GetComponent<PlayerJump>().enabled = true;
+        GetComponent<Player>().enabled = true;
+
+        GetComponent<Collider2D>().enabled = true;
+        GetComponent<CapsuleCollider2D>().enabled = true;
+
     }
 }
